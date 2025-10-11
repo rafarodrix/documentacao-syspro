e98f32b1c7068a8823c9dd5a0788cd4e08cec066
Rafael Rodrigues <rafaelrodrix@gmail.com>
2025-10-11 13:40:37 -0300

    ajsute autenticação zammad

---
+++ lib/auth.ts
@@ -0,0 +1,33 @@
+import { type AuthOptions } from "next-auth";
+
+export const authOptions: AuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    {
+      id: "zammad",
+      name: "Zammad",
+      type: "oauth",
+      authorization: {
+        url: `${process.env.ZAMMAD_URL}/oauth/authorize`,
+        params: { scope: "read" },
+      },
+      token: `${process.env.ZAMMAD_URL}/oauth/token`,
+      userinfo: `${process.env.ZAMMAD_URL}/api/v1/users/me`,
+      clientId: process.env.ZAMMAD_CLIENT_ID,
+      clientSecret: process.env.ZAMMAD_CLIENT_SECRET,
+      profile(profile) {
+        return {
+          id: profile.id.toString(),
+          name: `${profile.firstname} ${profile.lastname}`,
+          email: profile.email,
+          image: profile.image || null,
+        };
+      },
+    },
+  ],
+  pages: {
+    signIn: '/login',
+  },
+};
