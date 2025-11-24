eac6126c07c59fc8f142df9f614393ad7e1286d3
Rafael Rodrigues <rafaelrodrix@gmail.com>
2025-11-24 13:08:32 -0300

    ajuste autenticacao

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { prisma } from "@/lib/prisma"; 
+import { prisma } from "@/lib/prisma";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -8,7 +8,7 @@ export const auth = betterAuth({
   }),
 
   baseURL: process.env.BETTER_AUTH_URL,
-  
+
   trustedOrigins: [
     "http://localhost:3000",
     "https://ajuda.trilinksoftware.com.br"
@@ -16,7 +16,7 @@ export const auth = betterAuth({
 
   emailAndPassword: {
     enabled: true,
-    disableSignUp: true, 
+    disableSignUp: false,
   },
 
   session: {
