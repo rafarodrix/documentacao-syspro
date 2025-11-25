f20ad72976971bf9177f68f8aac10101fd0e3b0e
Rafael Rodrigues <rafaelrodrix@gmail.com>
2025-11-25 15:54:29 -0300

    ajustes documentação

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from "next/navigation";
+import { redirect } from 'next/navigation';
 
-export default function DocsIndexPage() {
-    redirect("/docs/manual");
+export default function Page() {
+    redirect('/docs/manual');
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-
-import './globals.css'; 
+import './globals.css';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
-import { Providers } from '@/components/providers'; 
+import { Providers } from '@/components/providers';
+import { RootProvider } from 'fumadocs-ui/provider';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -12,9 +12,11 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen bg-background text-foreground">
-        <Providers>
-          {children}
-        </Providers>
+        <RootProvider>
+          <Providers>
+            {children}
+          </Providers>
+        </RootProvider>
       </body>
     </html>
   );
