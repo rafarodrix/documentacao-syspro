85426173d850e56bad325dea2d1a6d998fe41756
Rafael Rodrigues <rafaelrodrix@gmail.com>
2025-11-28 10:35:10 -0300

    pequenas melhorias estrutura arquivos

--- src/app/(platform)/releases/layout.tsx
+++ src/app/(platform)/releases/layout.tsx
@@ -12,11 +12,7 @@ export default function ReleasesLayout({
             {/* 1. Reutilizando o Header do Site */}
             <SiteHeader />
 
-            {/* 2. Background Decorativo 
-         Mantive este background pois ele dá um toque visual sutil de "tech/grid" 
-         que é muito comum em páginas de Release Notes, diferenciando levemente do resto do site.
-         Se preferir o fundo 100% limpo igual ao site, basta remover esta div.
-      */}
+            {/* 2. Background Decorativo*/}
             <div className="fixed inset-0 -z-10 h-full w-full bg-background bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
                 <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-primary/20 opacity-20 blur-[100px]" />
             </div>
