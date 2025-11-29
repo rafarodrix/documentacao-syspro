6f5f9bbe6806d06959dcb649b71946bd92bd91b2
Rafael Rodrigues <rafaelrodrix@gmail.com>
2025-11-29 13:13:09 -0300

    ajuste route

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,16 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@/lib/auth";
+import { toNextJsHandler } from "better-auth/next-js";
 
-export async function GET(req: NextRequest) {
-    return NextResponse.json({ message: "A ROTA EXISTE! O Better Auth não foi chamado." });
-}
+const handlers = toNextJsHandler(auth);
 
-export async function POST(req: NextRequest) {
-    const body = await req.json().catch(() => ({}));
-    console.log("Recebi um POST em:", req.url);
-    console.log("Corpo:", body);
-
-    return NextResponse.json({
-        message: "POST recebido com sucesso!",
-        receivedBody: body
-    });
-}
+export const GET = handlers.GET;
+export const POST = handlers.POST;
