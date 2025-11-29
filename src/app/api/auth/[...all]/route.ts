28173b9aabaf0a0a89930bad3e93d924be0bd4b8
Rafael Rodrigues <rafaelrodrix@gmail.com>
2025-11-29 12:55:30 -0300

    ajuste route da auth

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,5 +1,9 @@
-
 import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
-export const { GET, POST } = toNextJsHandler(auth); 
+// Cria o handler do Better Auth
+const handlers = toNextJsHandler(auth);
+
+// Exporta explicitamente os métodos
+export const GET = handlers.GET;
+export const POST = handlers.POST;
