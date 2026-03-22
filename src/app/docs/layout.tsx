35372b36cfde1b9afff5d43026e25916e6b24669
Rafael Rodrigues <rafaelrodrix@gmail.com>
2026-03-22 09:50:59 -0300

    ajuste vizualização mobile

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -10,8 +10,10 @@ export default async function Layout({ children }: { children: ReactNode }) {
 
   return (
     <div className="flex min-h-screen flex-col bg-background">
-      <SiteHeader />
-      <main className="flex-1 min-h-0 [--fd-banner-height:64px]">
+      <div className="hidden md:block">
+        <SiteHeader />
+      </div>
+      <main className="flex-1 min-h-0 [--fd-banner-height:0px] md:[--fd-banner-height:64px]">
         <DocsLayout tree={source.pageTree} {...baseOptions}>
           {children}
         </DocsLayout>
