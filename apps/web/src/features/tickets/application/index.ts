587dc1359acdd5eb2c57474fac82f9a1af0f079e
Rafael Rodrigues <rafaelrodrix@gmail.com>
2026-04-07 20:13:30 -0300

    remoção zammadcompleta

--- apps/web/src/features/tickets/application/index.ts
+++ apps/web/src/features/tickets/application/index.ts
@@ -1,8 +1,4 @@
-﻿export * from './actions';
-export * from './dashboard';
-export * from './queries';
-export * from './zammad-global-settings-actions';
-export * from './zammad-global-settings-config';
-export * from './zammad-global-settings-server';
-export * from './utils';
-
+﻿export * from "./actions";
+export * from "./dashboard";
+export * from "./queries";
+export * from "./utils";
