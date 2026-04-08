18d8a5db493b68f8057cdcce60e696fab2f7ccf0
Rafael Rodrigues <rafaelrodrix@gmail.com>
2026-04-08 11:01:20 -0300

    ajustes

--- packages/database/prisma/migrations/20260408135225_melhorias_chatwoot_e_remocao_zammad/migration.sql
+++ packages/database/prisma/migrations/20260408135225_melhorias_chatwoot_e_remocao_zammad/migration.sql
@@ -1,5 +1,44 @@
--- AlterTable
-ALTER TABLE "company_ticket_email" RENAME CONSTRAINT "company_zammad_email_pkey" TO "company_ticket_email_pkey";
+DO $$
+BEGIN
+  IF to_regclass('"company_ticket_email"') IS NOT NULL THEN
+    IF EXISTS (
+      SELECT 1
+      FROM pg_constraint
+      WHERE conname = 'company_zammad_email_pkey'
+        AND conrelid = '"company_ticket_email"'::regclass
+    ) THEN
+      ALTER TABLE "company_ticket_email"
+      RENAME CONSTRAINT "company_zammad_email_pkey" TO "company_ticket_email_pkey";
+    END IF;
 
--- RenameForeignKey
-ALTER TABLE "company_ticket_email" RENAME CONSTRAINT "company_zammad_email_companyId_fkey" TO "company_ticket_email_companyId_fkey";
+    IF EXISTS (
+      SELECT 1
+      FROM pg_constraint
+      WHERE conname = 'company_zammad_email_companyId_fkey'
+        AND conrelid = '"company_ticket_email"'::regclass
+    ) THEN
+      ALTER TABLE "company_ticket_email"
+      RENAME CONSTRAINT "company_zammad_email_companyId_fkey" TO "company_ticket_email_companyId_fkey";
+    END IF;
+  ELSIF to_regclass('"company_zammad_email"') IS NOT NULL THEN
+    IF EXISTS (
+      SELECT 1
+      FROM pg_constraint
+      WHERE conname = 'company_zammad_email_pkey'
+        AND conrelid = '"company_zammad_email"'::regclass
+    ) THEN
+      ALTER TABLE "company_zammad_email"
+      RENAME CONSTRAINT "company_zammad_email_pkey" TO "company_ticket_email_pkey";
+    END IF;
+
+    IF EXISTS (
+      SELECT 1
+      FROM pg_constraint
+      WHERE conname = 'company_zammad_email_companyId_fkey'
+        AND conrelid = '"company_zammad_email"'::regclass
+    ) THEN
+      ALTER TABLE "company_zammad_email"
+      RENAME CONSTRAINT "company_zammad_email_companyId_fkey" TO "company_ticket_email_companyId_fkey";
+    END IF;
+  END IF;
+END $$;
