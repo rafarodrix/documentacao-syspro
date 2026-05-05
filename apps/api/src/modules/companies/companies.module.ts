10e4d59c76440594568d7e1922104755de34895f
Rafael Rodrigues <rafaelrodrix@gmail.com>
2026-05-05 12:07:30 -0300

    feat: update Companies and Trpc modules to use forwardRef for circular dependency resolution

--- apps/api/src/modules/companies/companies.module.ts
+++ apps/api/src/modules/companies/companies.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { CompaniesController } from './companies.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { CompaniesService } from './companies.service';
 import { CompaniesRouter } from './companies.router';
 import { ContactsModule } from '../contacts/contacts.module';
+import { TrpcModule } from '../trpc/trpc.module';
 
 @Module({
-  imports: [PrismaModule, ContactsModule],
+  imports: [PrismaModule, ContactsModule, forwardRef(() => TrpcModule)],
   controllers: [CompaniesController],
   providers: [CompaniesService, CompaniesRouter],
   exports: [CompaniesRouter],
