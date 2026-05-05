b2b426bd303e90e4f1b88dd7698d6cc5024dfc33
Rafael Rodrigues <rafaelrodrix@gmail.com>
2026-05-05 10:36:58 -0300

    feat: implement Prisma module and service for database connectivity and lifecycle management

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,28 +1,3 @@
-import { Injectable, OnModuleInit, OnModuleDestroy, Logger } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
-@Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  private readonly logger = new Logger(PrismaService.name);
-
-  constructor() {
-    super({
-      log: ['error', 'warn'],
-    });
-  }
-
-  async onModuleInit() {
-    try {
-      await this.$connect();
-      this.logger.log('Conectado ao Prisma via NestJS Standalone');
-    } catch (e) {
-      this.logger.error('Falha ao conectar usando Prisma', e);
-      throw e;
-    }
-  }
-
-  async onModuleDestroy() {
-    await this.$disconnect();
-    this.logger.log('Desconectado do Prisma');
-  }
-}
+export abstract class PrismaService extends PrismaClient {}
