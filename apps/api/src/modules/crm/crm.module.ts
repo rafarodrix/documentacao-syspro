478bce12b63fce8006b141b7248d29d4f2682344
Rafael Rodrigues <rafaelrodrix@gmail.com>
2026-05-20 09:12:15 -0300

    feat(crm): add TrpcCoreModule to CRM module imports

--- apps/api/src/modules/crm/crm.module.ts
+++ apps/api/src/modules/crm/crm.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
 import { AuthorizationModule } from '../authorization/authorization.module';
+import { TrpcCoreModule } from '../trpc/trpc-core.module';
 import { CrmService } from './crm.service';
 import { CrmRouter } from './crm.router';
 
 @Module({
-  imports: [PrismaModule, AuthModule, AuthorizationModule],
+  imports: [PrismaModule, AuthModule, AuthorizationModule, TrpcCoreModule],
   providers: [CrmService, CrmRouter],
   exports: [CrmService, CrmRouter],
 })
